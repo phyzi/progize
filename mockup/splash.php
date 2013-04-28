e3e9670f5e2ac72afc34853d348d6d8db45fee95
Nils Fischer <n.fischer@viwid.com>
2013-04-28 18:06:42 +0200

    added mockup folder to try designs

---
+++ mockup/splash.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ProGize Mockup</title>
+	<!-- Stylesheets -->
+	<link rel="stylesheet/less" type="text/css" href="style.less">
+	<script src="less.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div class="color_test_container hlmain_dark"></div>
+<div class="color_test_container hlmain_light"></div>
+<div class="color_test_container hlscnd_dark"></div>
+<div class="color_test_container hlscnd_light"></div>
+
+</body>
+</html>
