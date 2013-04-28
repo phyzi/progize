ed651af00f8e1d9d0a9ab50eef0f7f3c1162d764
Nils Fischer <n.fischer@viwid.com>
2013-04-28 21:35:11 +0200

    Added color scheme

--- mockup/splash.php
+++ mockup/splash.php
@@ -2,17 +2,34 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<title>ProGize Mockup</title>
+		
 	<!-- Stylesheets -->
 	<link rel="stylesheet/less" type="text/css" href="style.less">
+	
+	<!-- {less} -->
 	<script src="less.js" type="text/javascript"></script>
+	
 </head>
 <body>
 
-<div class="color_test_container hlmain_dark"></div>
-<div class="color_test_container hlmain_light"></div>
-<div class="color_test_container hlscnd_dark"></div>
-<div class="color_test_container hlscnd_light"></div>
+<div id="header">
+	
+	<div id="master_nav_container">
+		
+		<div class="item i_0">
+			<div class="item_text">ProGize</div>
+		</div>
+		<div class="item i_1">
+			<div class="item_text">Find Projects [-----]</div>
+		</div>
+		<div class="item i_2">
+		</div>
+		
+	</div>
+	
+</div>
 
 </body>
 </html>
