aa190e984141228ec66c65851ee452e81f130cdc
PancakeFriday <robin_eberhard@web.de>
2013-05-01 15:18:29 +0200

    adjusted database config file

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'rob2625';
+$db['default']['password'] = '';
 $db['default']['database'] = 'progize';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
