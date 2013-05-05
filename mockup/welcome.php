3dd640a39708e8768c909dfe3d4113b7e8e0ec7f
Nils Fischer <n.fischer@viwid.com>
2013-05-05 13:34:20 +0200

    be greeted with colors

---
+++ mockup/welcome.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>ProGize - Welcome [Mockup]</title>
+		
+	<!-- Stylesheets -->
+	<link rel="stylesheet/less" type="text/css" href="style.less">
+	
+	<!-- {less} -->
+	<script src="less.js" type="text/javascript"></script>
+	
+</head>
+<body>
+
+<div id="welcome_container">
+			
+	<div id="welcome_info_container">
+		
+		<div class="color_main_0" style="height: 2em"></div>
+		<div id="welcome_item_branding" class="color_main_1">
+			<div class="item_text">ProGize</div>
+		</div>
+		
+	</div>
+	
+	<div id="login_container">
+				
+		<div id="login_item_login" class="head_item">
+			LOGIN
+		</div>
+		
+		<div id="login_item_register" class="head_item"></div>
+				
+	</div>
+	
+</div>
+
+</body>
+</html>
