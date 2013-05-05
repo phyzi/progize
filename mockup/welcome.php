d66b233da01d2714eaeddfc3d78c9bc0a7996ce3
Nils Fischer <n.fischer@viwid.com>
2013-05-05 14:18:00 +0200

    show me some more colors

--- mockup/welcome.php
+++ mockup/welcome.php
@@ -20,18 +20,25 @@
 		
 		<div class="color_main_0" style="height: 2em"></div>
 		<div id="welcome_item_branding" class="color_main_1">
-			<div class="item_text">ProGize</div>
+			<div class="item_text">ProGize<br>[more info here]</div>
 		</div>
 		
 	</div>
 	
+	
 	<div id="login_container">
 				
-		<div id="login_item_login" class="head_item">
-			LOGIN
+		<div id="login_head_item_login" class="head_item">
+			<div class="item_text">LOGIN</div>
 		</div>
 		
-		<div id="login_item_register" class="head_item"></div>
+		<div class="form_item"></div>
+		
+		<div id="login_head_item_register" class="head_item">
+			<div class="item_text">REGISTER</div>
+		</div>
+
+		<div class="form_item"></div>
 				
 	</div>
 	
